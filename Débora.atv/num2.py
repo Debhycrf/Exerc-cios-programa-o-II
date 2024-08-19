c3918c4c3a7b26120be75b378e9ff077745d2239
Débora Souza <deboradesouzasantos189@gmail.com>
2024-08-19 09:54:51 -0300

    Delete Débora.atv directory

--- Débora.atv/num2.py
+++
@@ -1,5 +0,0 @@
-letra = input("Digite uma letra: ").lower()
-if letra in 'aeiou':
-    print(f"A letra '{letra}' é uma vogal.")
-else:
-    print(f"A letra '{letra}' é uma consoante.")
--- Débora.atv/num9.py
+++
@@ -1,8 +0,0 @@
-while True:
-    nome_usuario = input("Digite o nome de usuário: ").strip()
-    senha = input("Digite a senha: ").strip()
-    if senha == nome_usuario:
-        print("Erro: A senha não pode ser igual ao nome de usuário. Tente novamente.")
-    else:
-        print("Usuário e senha cadastrados com sucesso!")
-        break
