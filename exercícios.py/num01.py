612b175b06d2fec22c4744e64b326a7cf803d39d
Débora Souza <deboradesouzasantos189@gmail.com>
2024-08-19 09:55:24 -0300

    Delete exercícios.py directory

--- exercícios.py/num01.py
+++
@@ -1,4 +0,0 @@
-'''Escreva um código que receba o nome de uma pessoa e imprima uma
-saudação personalizada na tela com o nome do usuário.'''
-nome = input("Qual é o seu nome? ") 
-print("Olá, " + nome + "! Seja bem-vindo(a)!") 
